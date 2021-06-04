72fe1eb61c27bf7fca0067551bcfe8fcc023c4af
Taketo Sano <taketo1024@gmail.com>
2021-06-04 20:02:45 +0900

    add test.

--- Tests/SwmKRTests/KRHomologyTests.swift
+++ Tests/SwmKRTests/KRHomologyTests.swift
@@ -13,16 +13,35 @@ import SwmHomology
 
 class KRHomologyTests: XCTestCase {
     
-    typealias R = 𝐐
-    typealias KR = KRHomology<R>
-    
     override func setUp() {}
     
     override func tearDown() {}
     
     func testTrefoil() {
+        typealias R = 𝐐
+        
         let K = Link.load("3_1")!
-        let H = KR(K)
-        print(H.structure())
+        let H = KRHomology<R>(K)
+        let str = H.structure()
+        
+        XCTAssertEqual(str.count, 3)
+        XCTAssertEqual(str[[0, -4, 2]]!.rank, 1)
+        XCTAssertEqual(str[[-2, -2, 2]]!.rank, 1)
+        XCTAssertEqual(str[[2, -2, -2]]!.rank, 1)
+    }
+    
+    func testFigureEight() {
+        typealias R = 𝐐
+        
+        let K = Link.load("4_1")!
+        let H = KRHomology<R>(K)
+        let str = H.structure()
+        
+        XCTAssertEqual(str.count, 5)
+        XCTAssertEqual(str[[2, 0, -2]]!.rank, 1)
+        XCTAssertEqual(str[[-2, 0, 2]]!.rank, 1)
+        XCTAssertEqual(str[[0, 2, -2]]!.rank, 1)
+        XCTAssertEqual(str[[0, -2, 2]]!.rank, 1)
+        XCTAssertEqual(str[[0, 0, 0]]!.rank, 1)
     }
 }
