9f97306fcd7b61e99bea49b9bdae4800e30cf039
Taketo Sano <taketo1024@gmail.com>
2021-10-01 18:01:29 +0900

    support for no-symmetry-mode.

--- Sources/SwmKR/Basics/KRHomology.swift
+++ Sources/SwmKR/Basics/KRHomology.swift
@@ -40,9 +40,6 @@ public struct KRHomology<R: HomologyCalculatable>: GradedModuleStructureType {
             if !iRange.contains(i) || !jRange.contains(j) || !kRange.contains(k) || !kRange.contains(k + 2 * i) {
                 return .zeroModule
             }
-            if i > 0 {
-                return self[-i, j, k + 2 * i]
-            }
         }
         guard let (h, v, s) = ijk2hvs(i, j, k) else {
             return .zeroModule
@@ -77,19 +74,14 @@ public struct KRHomology<R: HomologyCalculatable>: GradedModuleStructureType {
 
         let v0 = Cube.Coords.zeros(length: n)
         let base = KR.baseGrading(link: L, hCoords: v0, vCoords: v0)
-        let gradingShift: KR.Grading = [-w + s - 1, w + s - 1, w - s + 1]
+        let gradingShift = KR.Grading(-w + s - 1, w + s - 1, w - s + 1)
         
         return base + gradingShift
     }
     
     public var levelRange: ClosedRange<Int> {
         let n = L.crossingNumber
-        let s = L.numberOfSeifertCircles
-        
-        let l0 = -2 * n
-        let l1 = (-3 * n + s - 1) / 2
-
-        return l0 ... l1
+        return -2 * n ... -n
     }
     
     public var iRange: ClosedRange<Int> {
@@ -161,99 +153,6 @@ public struct KRHomology<R: HomologyCalculatable>: GradedModuleStructureType {
     public func clearCache() {
         horizontalHomologyCache.clear()
         verticalHomologyCache.clear()
-    }
-}
-
-// convenience methods
-extension KRHomology {
-    public func structure(restrictedTo: (Int, Int, Int) -> Bool = { (_, _, _) in true }) -> [KR.Grading : ModuleStructure<KR.TotalModule<R>>] {
-        typealias E = (KR.Grading, ModuleStructure<KR.TotalModule<R>>)
-        
-        let n = L.crossingNumber
-        let elements = levelRange.flatMap { s -> [E] in
-            ((0 ... n) * (0 ... n)).compactMap { (h, v) -> E? in
-                let (i, j, k) = hvs2ijk(h, v, s)
-                if restrictedTo(i, j, k) {
-                    let H = self[i, j, k]
-                    return !H.isZero ? ([i, j, k], H) : nil
-                } else {
-                    return nil
-                }
-            }
-        }
-        return Dictionary(elements)
-    }
-
-    public func table(restrictedTo: (Int, Int, Int) -> Bool = { (_, _, _) in true }) -> String {
-        typealias qPoly = KR.qPolynomial<𝐙>
-        let str = structure(restrictedTo: restrictedTo)
-        let table = str
-            .group { (ijk, _) in
-                IntList<_2>(ijk[1], ijk[2])
-            }
-            .mapValues{ list in
-                qPoly(elements: list.map{ (ijk, V) in
-                    (ijk[0], V.rank)
-                })
-            }
-        
-        return Format.table(
-            rows: table.keys.map{ $0[1] }.uniqued().sorted().reversed(), // k
-            cols: table.keys.map{ $0[0] }.uniqued().sorted(), // j
-            symbol: "k\\j",
-            printHeaders: true
-        ) { (k, j) -> String in
-            let q = table[[j, k]] ?? .zero
-            return !q.isZero ? "\(q)" : ""
-        }
-    }
-    
-    public func table(i: Int? = nil, j: Int? = nil, k: Int? = nil) -> String {
-        table { (i1, j1, k1) in
-            return
-                (i.flatMap{ $0 == i1 } ?? true)
-             && (j.flatMap{ $0 == j1 } ?? true)
-             && (k.flatMap{ $0 == k1 } ?? true)
-        }
-    }
-    
-    public func printTable(restrictedTo r: (Int, Int, Int) -> Bool = { (_, _, _) in true }) {
-        print(table(restrictedTo: r))
-    }
-    
-    public func printTable(i: Int? = nil, j: Int? = nil, k: Int? = nil) {
-        print(table(i: i, j: j, k: k))
-    }
-    
-    public var gradedEulerCharacteristic: KR.qaPolynomial<𝐙> {
-        qaPolynomial(structure())
-    }
-    
-    public var lowestQPart: KR.qaPolynomial<𝐙> {
-        let str = structure { (i, _, _) in i == iRange.lowerBound }
-        return qaPolynomial(str)
-    }
-    
-    public var highestQPart: KR.qaPolynomial<𝐙> {
-        let str = structure { (i, _, _) in i == iRange.upperBound }
-        return qaPolynomial(str)
-    }
-    
-    public var lowestAPart: KR.qaPolynomial<𝐙> {
-        let str = structure { (_, j, _) in j == jRange.lowerBound }
-        return qaPolynomial(str)
-    }
-    
-    public var highestAPart: KR.qaPolynomial<𝐙> {
-        let str = structure { (_, j, _) in j == jRange.upperBound }
-        return qaPolynomial(str)
-    }
-    
-    private func qaPolynomial(_ structure: [KR.Grading : ModuleStructure<KR.TotalModule<R>>]) -> KR.qaPolynomial<𝐙> {
-        .init(elements: structure.map { (g, V) in
-            let (i, j, k) = (g[0], g[1], g[2])
-            return ([i, j], (-1).pow( (k - j) / 2) * V.rank )
-        })
     }
 }
 
